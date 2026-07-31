7b69b37b464f028694fbea37585ee58925cdb405
M0JXD <jamiedrinkell@gmail.com>
2026-07-31 21:33:11 +0100

    Workaround set_theme nil error for now

--- modules/theme_mgr/init.lua
+++ modules/theme_mgr/init.lua
@@ -97,8 +97,8 @@ function M.theme_view(view_reset, view)
 		theme = UI == 'terminal' and M.theme.term or (_THEME == 'dark') and M.theme.dark or
 			M.theme.light
 	end
-	-- TODO: When splitting this fails saying set_theme is a nil value, but only on Windows??
-	view:set_theme(theme, {font = M.font.family, size = M.font.size})
+	-- TODO: When splitting this fails saying set_theme is a nil value, but only on Qt?
+	if view.set_theme then view:set_theme(theme, {font = M.font.family, size = M.font.size}) end
 end
 
 function M.theme_all_views(view_reset)
