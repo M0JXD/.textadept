f93e5fb4079c62bfd4b0eb0a2f323ea099e28646
M0JXD <jamiedrinkell@gmail.com>
2026-08-09 21:32:17 +0100

    Add LDoc to quick_open

--- modules/quick_open/README.md
+++ modules/quick_open/README.md
@@ -1,27 +1,112 @@
-# Quick Open Module
+# Textadept Quick Open
 
-Quickly open a Terminal or File Browser at the currently opened file's path.
-It's based on https://github.com/orbitalquark/textadept/wiki/TerminalHere
+Textadept Quick Open
 
-By default, it will use:
+Quickly open a terminal, file explorer or git client at the current buffer's file path.
 
-- `explorer.exe` and `cmd.exe` on Windows (I'm unsure how well it will work for different explorers and terminals on Windows).
-- On Linux/BSD:
-    - xdg-open to open a file browser on Linux/BSD.
-    - The suspected default terminal for the currently detected desktop environment.
-- There's also an option to launch a TUI Git client with a fullscreen terminal. The default is Lazygit.
+It's based on <https://github.com/orbitalquark/textadept/wiki/TerminalHere>.
 
-I don't have any Apple devices so I'm unable to implement for macOS.
-If setting a custom terminal, as the directory argument are sometimes like `--working-directory=` you might need to add the trailing whitespace.
+I don't have any Apple devices so have not implemented for macOS.
+If setting a custom terminal, as the directory argument may of the form `--working-directory=`
+you might need to add the trailing whitespace.
 
-Example usage:
+Only for GUI Textadept, returns an empty table if used in the terminal version.
+
+<a id="quick_open.bindings"></a>
+## `quick_open.bindings`
+
+Bindings to use to launch applications at current path.
+
+Fields:
+- `terminal`: Binding to launch Terminal. Default value is `'ctrl+T'`.
+- `explorer`: Binding to launch File Explorer. Default value is `'ctrl+E'`.
+- `git_client`: Binding to launch Git Client. Default value is `'ctrl+G'`.
+
+Usage:
 
 ```lua
-local quick_open = require('quick_open')
-quick_open.terminal = 'cool-retro-term'
-quick_open.term_dir_arg = '--workdir '
-quick_open.term_max_arg = '--fullscreen'
-quick_open.explorer = 'nautilus'
-quick_open.git_client = 'gitui'
 quick_open.bindings.terminal = 'alt+T'
+```
+
+<a id="quick_open.explorer"></a>
+## `quick_open.explorer`
+
+Command to open a file explorer.
+
+Default value is `'explorer.exe'` on Windows and `'xdg-open'` on Linux/BSD.
+
+Usage:
+
+```lua
+quick_open.explorer = 'nautilus'
+```
+
+<a id="quick_open.git_client"></a>
+## `quick_open.git_client`
+
+Git client to open.
+
+Default value is `'lazygit'`.
+
+Usage:
+
+```lua
+quick_open.git_client = 'gitui'
+```
+
+<a id="quick_open.openFileBrowserHere"></a>
+## `quick_open.openFileBrowserHere`()
+
+Open a file explorer at the current buffer's directory.
+
+<a id="quick_open.openGitClientHere"></a>
+## `quick_open.openGitClientHere`()
+
+Open a git client at the current buffer's directory.
+
+<a id="quick_open.openTerminalHere"></a>
+## `quick_open.openTerminalHere`(*arg*)
+
+Open a terminal at the current buffer's directory.
+
+Parameters:
+- *arg*: Command to pass to terminal emulator (used to open git client).
+
+<a id="quick_open.term_dir_arg"></a>
+## `quick_open.term_dir_arg`
+
+Argument to pass a starting directory to the terminal emulator.
+
+The module attempts to detect what is correct for your OS/Desktop.
+
+Usage:
+
+```lua
+quick_open.term_dir_arg = '--workdir ' -- Trailing space might be needed
+```
+
+<a id="quick_open.term_max_arg"></a>
+## `quick_open.term_max_arg`
+
+Argument to maximise the terminal emulator.
+
+The module attempts to detect what is correct for your OS/Desktop.
+
+Usage:
+
+```lua
+quick_open.term_max_arg = '--fullscreen'
+```
+
+<a id="quick_open.terminal"></a>
+## `quick_open.terminal`
+
+Terminal emulator to open.
+
+The module attempts to detect what is correct for your OS/Desktop.
+
+Usage:
+
+```lua
+quick_open.terminal = 'cool-retro-term'
 ```
--- modules/quick_open/init.lua
+++ modules/quick_open/init.lua
@@ -1,7 +1,16 @@
--- Copyright 2025 Jamie Drinkell. MIT License.
--- Quick open module to open a terminal, file explorer etc. at the current file path
--- Only for GUI version.
+-- Copyright 2025-2026 Jamie Drinkell. MIT License.
 
+--- Textadept Quick Open
+--
+-- Quickly open a terminal, file explorer or git client at the current buffer's file path.
+-- It's based on <https://github.com/orbitalquark/textadept/wiki/TerminalHere>.
+--
+-- I don't have any Apple devices so have not implemented for macOS.
+-- If setting a custom terminal, as the directory argument may of the form `--working-directory=`
+-- you might need to add the trailing whitespace.
+--
+-- Only for GUI Textadept, returns an empty table if used in the terminal version.
+-- @module quick_open
 local M = {}
 
 if UI == 'terminal' then return M end
@@ -9,24 +18,52 @@ if UI == 'terminal' then return M end
 local desktop = os.getenv('XDG_CURRENT_DESKTOP')
 if desktop == nil then desktop = '' end
 
+--- Bindings to use to launch applications at current path.
+-- @usage quick_open.bindings.terminal = 'alt+T'
 M.bindings = {
-	terminal = 'ctrl+T', explorer = 'ctrl+E', git_client = 'ctrl+G'
+	terminal = 'ctrl+T', -- Binding to launch Terminal. Default value is `'ctrl+T'`.
+	explorer = 'ctrl+E', -- Binding to launch File Explorer. Default value is `'ctrl+E'`.
+	git_client = 'ctrl+G' -- -- Binding to launch Git Client. Default value is `'ctrl+G'`.
 }
 
--- Most GTK terminals use these
+--- Terminal emulator to open.
+-- The module attempts to detect what is correct for your OS/Desktop.
+-- @usage quick_open.terminal = 'cool-retro-term'
+M.terminal = OS == 'windows' and 'cmd.exe /f:on' or 'xterm -hold'
+
+--- Argument to maximise the terminal emulator.
+-- The module attempts to detect what is correct for your OS/Desktop.
+-- @usage quick_open.term_max_arg = '--fullscreen'
+M.term_max_arg = '-fullscreen'
+
+--- Argument to pass a starting directory to the terminal emulator.
+-- The module attempts to detect what is correct for your OS/Desktop.
+-- @usage quick_open.term_dir_arg = '--workdir ' -- Trailing space might be needed
 M.term_dir_arg = '--working-directory='
-M.term_max_arg = '--maximize'
+-- TODO: xterm doesn't really support giving a directory at startup?
 
 if desktop:match('Cinnamon') then
 	M.terminal = 'gnome-terminal'
+	M.term_max_arg = '--maximize'
 elseif desktop:match('XFCE') then
 	M.terminal = 'xfce4-terminal'
+	M.term_max_arg = '--maximize'
 elseif desktop:match('MATE') then
 	M.terminal = 'mate-terminal'
+	M.term_max_arg = '--maximize'
 elseif desktop:match('LXDE') then
 	M.terminal = 'lxterminal'
+	M.term_max_arg = '--maximize'
 elseif desktop:match('GNOME') then
 	M.terminal = 'kgx'
+	M.term_max_arg = '--maximize'
+	-- Check if Ubuntu because it still uses gnome-terminal
+	local handle = io.popen("lsb_release -si 2>/dev/null")
+	if handle then
+		local result = handle:read("*a")
+		if result:find("Ubuntu") then M.terminal = 'gnome-terminal' end
+		handle:close()
+	end
 elseif desktop:match('KDE') then
 	M.terminal = 'konsole'
 	M.term_dir_arg = '--workdir '
@@ -37,22 +74,22 @@ elseif desktop:match('ENLIGHTENMENT') then
 	M.term_max_arg = '-M'
 elseif desktop:match('LXQt') then
 	M.terminal = 'qterminal'
+	M.term_max_arg = '--maximize'
 	-- Presumably the same as lxterminal?
-else
-	-- xterm doesn't really support giving a directory at startup?
-	M.terminal = 'xterm -hold'
-	M.term_max_arg = '-fullscreen'
 end
 
-M.explorer = 'xdg-open'
-M.git_client = 'lazygit'
+--- Command to open a file explorer.
+-- Default value is `'explorer.exe'` on Windows and `'xdg-open'` on Linux/BSD.
+-- @usage quick_open.explorer = 'nautilus'
+M.explorer = OS == 'windows' and 'explorer.exe' or 'xdg-open'
 
-if OS == 'windows' then
-	M.terminal = 'cmd.exe /f:on'
-	M.explorer = 'explorer.exe'
-	M.git_client = M.git_client .. '.exe'
-end
+--- Git client to open.
+-- Default value is `'lazygit'`.
+-- @usage quick_open.git_client = 'gitui'
+M.git_client = OS == 'windows' and 'lazygit' or 'lazygit.exe'
 
+--- Open a terminal at the current buffer's directory.
+-- @param arg[opt] Command to pass to terminal emulator (used to open git client).
 function M.openTerminalHere(arg)
 	local argString = '~'
 	if OS ~= 'windows' then
@@ -78,6 +115,7 @@ function M.openTerminalHere(arg)
 	end
 end
 
+--- Open a file explorer at the current buffer's directory.
 function M.openFileBrowserHere()
 	local pathString = '~'
 	if OS ~= 'windows' then
@@ -90,6 +128,7 @@ function M.openFileBrowserHere()
 	end
 end
 
+--- Open a git client at the current buffer's directory.
 function M.openGitClientHere()
 	M.openTerminalHere(M.git_client)
 end
