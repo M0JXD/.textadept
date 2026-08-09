79c1f127cd78f32f8ef7f09b36972cf3ccc9add2
M0JXD <jamiedrinkell@gmail.com>
2026-08-09 22:18:28 +0100

    Add LDoc to theme_mgr

--- modules/theme_mgr/README.md
+++ modules/theme_mgr/README.md
@@ -1,16 +1,32 @@
-# Theme Manager Module
+# Theme manager for Textadept
 
-To truly change themes with the system in Textadept [is a bit complicated if you don't want to override the default theme files](https://github.com/orbitalquark/textadept/issues/602#issuecomment-2758753214).  
-Theme manager is a module for setting themes that switch with the system in the GUI version, and carefully applies theme aspects depending on system limitations, such as:
+Theme manager for Textadept
 
-- If a requested font is missing, it will opt to use Textadept's default font instead of the OS default font. The matching is very simple so if you've put 'Comic Sans' instead of 'Comic Sans MS' it can still fail to fallback properly.
-- When changing themes, it resets styles to Textadept's defaults to avoid spurious behaviours like current line backgrounds persisting on themes that don't have them.
-- Since 12.7, Textadept supports arbitrary RGB colours in the terminal version, which means many GUI themes also work in terminals with true-colour support. The module will attempt to detect if a terminal has true-colour support and fallback to the default terminal theme if necessary.
-- Theme manager allows for per lexer theming, see the example usage.
-- If using a GTK2 build, it attempts to detect if the system GTK theme is a dark one so it can apply your chosen dark theme. There is no mode changed support for GTK2 (but I don't think any GTK2 DEs had such capability anyway). NB: The check relies on `textadept-gtk` being in your `PATH`.
-- I've added [@kbarni's theme selector](https://github.com/orbitalquark/textadept/pull/690#issue-3996335774) too just for fun!
+To truly change themes with the system in Textadept
+[is a bit complicated if you don't want to override the default theme files][1].
+
+Theme manager is a module for setting themes that switch with the system in the GUI version,
+and carefully applies theme aspects depending on system limitations.
+
+If a font is missing, it will opt to use Textadept's default font instead of the OS default font.
+
+When changing themes, it resets styles to avoid spurious behaviours like line backgrounds
+persisting on themes that don't have them.
+
+Since 12.7, Textadept supports arbitrary RGB colours in the terminal version, which means many
+GUI themes also work in terminals with true-colour support. The module detects if the current
+terminal is capable to otherwise fallback to the default theme.
+
+If using the GTK2 build, it attempts to detect if the system GTK theme is a dark one so it can
+apply your chosen dark theme. There is no mode changed support for GTK2
+(I don't think any GTK2 DEs had such capability anyway).
+NB: The check relies on `textadept-gtk` being in your `PATH`.
+
+I've added [@kbarni's theme selector][2] too just for fun!
 
 By default, Theme Manager uses Textadept's default themes and settings.
+Theme manager allows for per lexer theming, including separate light and dark themes.
+
 Example usage:
 
 ```lua
@@ -18,9 +34,67 @@ local theme_mgr = require('theme_mgr')
 theme_mgr.theme.light = 'xed-light'
 theme_mgr.theme.dark = 'ayu-evolve'
 theme_mgr.theme.term = 'catppuccin-latte'
-theme_mgr.font.family= 'Comic Sans MS'
-theme_mgr.font.size = 14
-theme_mgr.theme.python = 'xed-dark' -- Set a lexer specific theme
-theme_mgr.theme.markdown = {'ayu-mirage', 'catppuccin-macchiato'}  -- Light then dark
--- theme_mgr() -- Call the module if you need themes set before events.INITIALIZED
+theme_mgr.font.family = 'Comic Sans MS'
+theme_mgr() -- Call the module if you need themes set before events.INITIALIZED
 ```
+
+[1]: https://github.com/orbitalquark/textadept/issues/602#issuecomment-2758753214
+[2]: https://github.com/orbitalquark/textadept/pull/690#issue-3996335774
+
+
+<a id="theme_mgr.font"></a>
+## `theme_mgr.font`
+
+Table of font properties to use.
+
+Fields:
+- `family`:  The font to use. Default value is the same as Textadept's default for each OS.
+- `size`:  The font size to use. Default value is `12`.
+
+<a id="theme_mgr.select_theme"></a>
+## `theme_mgr.select_theme`(*mode*)
+
+Theme selector adapted from @kbarni
+Lists themes to user and allows them to select what to apply.
+
+Parameters:
+- *mode*:  Whether theme should be applied to light or dark mode.
+
+<a id="theme_mgr.theme"></a>
+## `theme_mgr.theme`
+
+Map of lexers with the themes to use for them, or light/dark/term if not.
+
+Fields:
+- `light`:  Which light theme to use when there's no specified lexer theme. Default value is `'light'`.
+- `dark`:  Which dark theme to use when there's no specified lexer theme. Default value is `'dark'`.
+- `term`:  Which terminal theme to use when there's no specified lexer theme. Default value is `'term'`.
+
+Usage:
+
+```lua
+theme_mgr.theme.python = 'xed-dark' -- Set a lexer specific theme
+theme_mgr.theme.markdown = {'ayu-mirage', 'catppuccin-macchiato'} -- Light then dark lexer themes
+```
+
+<a id="theme_mgr.theme_all_views"></a>
+## `theme_mgr.theme_all_views`(*view_reset*)
+
+Applies theme to all views.
+
+Parameters:
+- *view_reset*:  Whether to reset each view before applying the theme.
+
+<a id="theme_mgr.theme_command_entry"></a>
+## `theme_mgr.theme_command_entry`()
+
+Applies theme to command entry.
+
+<a id="theme_mgr.theme_view"></a>
+## `theme_mgr.theme_view`(*view_reset*, *view*)
+
+Apply selected themes to a view.
+
+Parameters:
+- *view_reset*:  Whether to reset the view before theming it.
+- *view*:  View to theme.
--- modules/theme_mgr/init.lua
+++ modules/theme_mgr/init.lua
@@ -1,17 +1,75 @@
 -- Copyright 2025-2026 Jamie Drinkell. MIT License.
--- Theme manager for Textadept.
--- Allow system switching and automatic detection/application of what's best in some environments.
--- Handy if you don't want to override the default themes to achieve this.
--- (Leaving a fallback for when you edit your themes and crash everything...)
 
-local M = {theme = {}, font = {}, mt = {}}
+--- Theme manager for Textadept
+--
+-- To truly change themes with the system in Textadept
+-- [is a bit complicated if you don't want to override the default theme files][1].
+-- Theme manager is a module for setting themes that switch with the system in the GUI version,
+-- and carefully applies theme aspects depending on system limitations.
+--
+-- If a font is missing, it will opt to use Textadept's default font instead of the OS default font.
+--
+-- When changing themes, it resets styles to avoid spurious behaviours like line backgrounds
+-- persisting on themes that don't have them.
+--
+-- Since 12.7, Textadept supports arbitrary RGB colours in the terminal version, which means many
+-- GUI themes also work in terminals with true-colour support. The module detects if the current
+-- terminal is capable to otherwise fallback to the default theme.
+--
+-- If using the GTK2 build, it attempts to detect if the system GTK theme is a dark one so it can
+-- apply your chosen dark theme. There is no mode changed support for GTK2
+-- (I don't think any GTK2 DEs had such capability anyway).
+-- NB: The check relies on `textadept-gtk` being in your `PATH`.
+--
+-- I've added [@kbarni's theme selector][2] too just for fun!
+--
+-- By default, Theme Manager uses Textadept's default themes and settings.
+-- Theme manager allows for per lexer theming, including separate light and dark themes.
+--
+-- Example usage:
+--
+-- ```lua
+-- local theme_mgr = require('theme_mgr')
+-- theme_mgr.theme.light = 'xed-light'
+-- theme_mgr.theme.dark = 'ayu-evolve'
+-- theme_mgr.theme.term = 'catppuccin-latte'
+-- theme_mgr.font.family = 'Comic Sans MS'
+-- theme_mgr() -- Call the module if you need themes set before events.INITIALIZED
+-- ```
+--
+-- [1]: https://github.com/orbitalquark/textadept/issues/602#issuecomment-2758753214
+-- [2]: https://github.com/orbitalquark/textadept/pull/690#issue-3996335774
+--
+-- @module theme_mgr
+local M = {}
+
+local mt = {}
 local default_font = (OS == 'windows') and 'Consolas' or (OS == 'macos') and 'Monaco' or 'Monospace'
-M.theme.light = 'light'
-M.theme.dark = 'dark'
-M.theme.term = 'term'
-M.font.size = 12
-M.font.family = default_font
 
+--- Map of lexers with the themes to use for them, or light/dark/term if not.
+-- @field light Which light theme to use when there's no specified lexer theme. Default value is `'light'`.
+-- @field dark Which dark theme to use when there's no specified lexer theme. Default value is `'dark'`.
+-- @field term Which terminal theme to use when there's no specified lexer theme. Default value is `'term'`.
+-- @usage theme_mgr.theme.python = 'xed-dark' -- Set a lexer specific theme
+-- @usage theme_mgr.theme.markdown = {'ayu-mirage', 'catppuccin-macchiato'} -- Light then dark lexer themes
+M.theme = {
+	light = 'light',
+	dark = 'dark',
+	term = 'term'
+}
+
+--- Table of font properties to use.
+-- @field family The font to use. Default value is the same as Textadept's default for each OS.
+-- @field size The font size to use. Default value is `12`.
+M.font = {
+	family = default_font,
+	size = 12
+}
+
+--- Checks if a font is installed on the system.
+-- The matching is very simple so if you've put 'Comic Sans' instead of 'Comic Sans MS' it can fail.
+-- @param font Font name to check exists.
+-- @return `nil` if not found, otherwise a truthy value.
 local function check_font(font)
 	local font_check_cmd
 	if OS == 'windows' then
@@ -33,6 +91,8 @@ local function check_font(font)
 	return list:match(font)
 end
 
+--- Checks current terminal for TUI version can support a colourful theme.
+-- @return `true` if terminal can support colourful themes.
 local function check_term()
 	-- GNOME Terminal, Tilix, Konsole, XFCE, LXDE etc. all report 'xterm-256color'
 	-- Alacritty reports 'alacritty'
@@ -44,6 +104,8 @@ local function check_term()
 	return true
 end
 
+--- If running the GTK2 version, checks if it should be using a dark theme.
+-- @return A truthy value if a dark theme should be forcefully applied.
 local function check_gtk2_dark()
 	local path = os.spawn('which textadept-gtk'):read('a'):match("^%s*(.-)%s*$")
 	if os.execute('ldd ' .. path .. ' | grep gtk-x11-2') then
@@ -52,7 +114,8 @@ local function check_gtk2_dark()
 	end
 end
 
--- Reset some commonly adjusted things that cause problems when switching themes
+--- Reset some commonly adjusted things that cause problems when switching themes
+-- @param view_to_reset View that should have values reset.
 local function reset_view(view_to_reset)
 	if UI ~= 'terminal' or (UI == 'terminal' and not (M.theme.term == 'term')) then
 		view_to_reset:style_reset_default()
@@ -83,6 +146,9 @@ local function reset_view(view_to_reset)
 	end
 end
 
+--- Apply selected themes to a view.
+-- @param view_reset Whether to reset the view before theming it.
+-- @param view View to theme.
 function M.theme_view(view_reset, view)
 	if view_reset then reset_view(view) end
 	local theme
@@ -101,10 +167,13 @@ function M.theme_view(view_reset, view)
 	if view.set_theme then view:set_theme(theme, {font = M.font.family, size = M.font.size}) end
 end
 
+--- Applies theme to all views.
+-- @param view_reset Whether to reset each view before applying the theme.
 function M.theme_all_views(view_reset)
 	for _, view in ipairs(_VIEWS) do M.theme_view(view_reset, view) end
 end
 
+--- Applies theme to command entry.
 function M.theme_command_entry()
 	local theme = UI == 'terminal' and M.theme.term or (_THEME == 'dark') and M.theme.dark or
 		M.theme.light
@@ -113,6 +182,7 @@ function M.theme_command_entry()
 	end)
 end
 
+--- Checks done at startup for fonts and theme capabilities.
 local function init_checks()
 	if M.font.family ~= default_font and not check_font(M.font.family) then
 		M.font.family = default_font
@@ -157,7 +227,9 @@ events.connect(events.INITIALIZED, function()
 	end
 end)
 
--- Theme selector by @kbarni! https://github.com/orbitalquark/textadept/pull/690#issue-3996335774
+--- Theme selector adapted from @kbarni
+-- Lists themes to user and allows them to select what to apply.
+-- @param mode Whether theme should be applied to light or dark mode.
 function M.select_theme(mode)
 	local themes = {}
 	for _, dir in ipairs{_USERHOME .. '/themes', _HOME .. '/themes'} do
@@ -201,14 +273,14 @@ function M.select_theme(mode)
 	end
 end
 
-M.mt.__call = function()
+mt.__call = function()
 	events.disconnect(events.INITIALIZED, init)
 	init_checks()
 	local theme = UI == 'terminal' and M.theme.term or (_THEME == 'dark') and M.theme.dark or
 		M.theme.light
 	view:set_theme(theme, {font = M.font.family, size = M.font.size})
 end
-setmetatable(M, M.mt)
+setmetatable(M, mt)
 
 _L['Change Theme...'] = 'Change _Theme...'
 _L['Select Light Theme'] = 'Select _Light Theme'
