225ccd66a097a7a777e9f5a241e13a464531513c
M0JXD <jamiedrinkell@gmail.com>
2026-09-24 04:58:53 +0100

    Run formatter and ensure "else" is optional

--- lexers/typst.lua
+++ lexers/typst.lua
@@ -54,11 +54,11 @@ local assignment = varwithdot * P' = ' * assignables * (P' ' * (operators * ' ' 
 local let_bind = P'let ' * varwithdot * P' = ' *
 	(parenthesized + assignables * (P' ' * (operators * ' ' * assignables))^0)
 local named_func = P'let ' * func * P' = ' * (parenthesized + code_block + lexer.to_eol())
-local conditional_if = P'if ' * assignables * (P' ' * (operators * ' ' * assignables))^0 * ' ' * code_content
-local conditional = conditional_if * P' else ' * (conditional_if * (P' else '^-1) + code_content)^0
-local for_loop = P'for ' * varwithdot * P' in ' * assignables * ' ' * code_content
-local while_loop = P'while ' * varwithdot * ' ' * operators * ' ' * assignables * ' ' *
+local conditional_if = P'if ' * assignables * (P' ' * (operators * ' ' * assignables))^0 * ' ' *
 	code_content
+local conditional = conditional_if * (P' else ' * conditional_if * (P' else '^-1) + code_content)^0
+local for_loop = P'for ' * varwithdot * P' in ' * assignables * ' ' * code_content
+local while_loop = P'while ' * varwithdot * ' ' * operators * ' ' * assignables * ' ' * code_content
 local set_rule = P'set ' * func
 local set_if = set_rule * conditional
 local show = P'show' * (': ' + (' ' * varwithdot)) * S': '^-2 * (func + set_rule + varwithdot)
@@ -67,7 +67,8 @@ local import = P'import ' * string_type * ((P': ' + P' as ') * lexer.to_eol())^-
 
 local expression = '#' *
 	(code_block + parenthesized + content + func + let_bind + named_func + set_if + set_rule +
-		for_loop + while_loop + conditional + assignment + include + import + show + varwithdot) * P';'^-1
+		for_loop + while_loop + conditional + assignment + include + import + show + varwithdot) *
+	P';'^-1
 
 lex:add_rule('expression', lex:tag(lexer.EMBEDDED, expression))
 
